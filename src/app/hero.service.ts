bdaf5fb8ff8d4520cfe4285382dc6ad7095476a4
maduar <maduar86@gmail.com>
2017-02-23 10:14:17 +0800

    add router

---
+++ src/app/hero.service.ts
@@ -0,0 +1,21 @@
+import { Injectable }    from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import { Hero } from './hero';
+
+import { HEROES } from './mock-heroes';
+import { Location }                 from '@angular/common';
+
+@Injectable()
+export class HeroService {
+    getHeroes(): Promise<Hero[]> {
+        return Promise.resolve(HEROES);
+    }
+
+    getHero(id: number): Promise<Hero> {
+        return this.getHeroes()
+            .then(heroes => heroes.find(hero => hero.id === id));
+    }
+}
